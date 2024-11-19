282da029f074109ff266d4eb43a9cc84802deeca
axperty <kevingelhorn0@gmail.com>
2024-11-19 15:57:05 -0300

    General changes.

--- src/main/java/com/axperty/stackedblocks/registry/CreativeTabRegistry.java
+++ src/main/java/com/axperty/stackedblocks/registry/CreativeTabRegistry.java
@@ -1,56 +1,60 @@
 package com.axperty.stackedblocks.registry;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import com.axperty.stackedblocks.StackedBlocks;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.neoforged.fml.ModList;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredRegister;
-
 
 public class CreativeTabRegistry {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, StackedBlocks.MOD_ID);
+    public static final ItemGroup STACKEDBLOCKS_ITEMGROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.tryParse(StackedBlocks.MODID + ":" + "stackedblocks_itemgroup"),
+            FabricItemGroup.builder()
+                    .displayName(Text.translatable("itemGroup.stackedblocks"))
+                    .icon(() -> new ItemStack(BlockRegistry.STACKED_MELONS))
+                    .entries((displayContext, entries) -> {
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> STACKEDBLOCKS_TAB = CREATIVE_MODE_TABS.register("stackedblocks_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.stackedblocks"))
-            .icon(() -> ItemRegistry.STACKED_MELONS_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
+                        entries.add(BlockRegistry.STACKED_STONE_BLOCKS);
+                        entries.add(BlockRegistry.STACKED_COBBLESTONE_BLOCKS);
+                        entries.add(BlockRegistry.STACKED_NETHERRACK_BLOCKS);
+                        entries.add(BlockRegistry.STACKED_BRICKS);
 
-                output.accept(BlockRegistry.STACKED_STONE_BLOCKS.get());
-                output.accept(BlockRegistry.STACKED_COBBLESTONE_BLOCKS.get());
-                output.accept(BlockRegistry.STACKED_NETHERRACK_BLOCKS.get());
-                output.accept(BlockRegistry.STACKED_BRICKS.get());
+                        if (FabricLoader.getInstance().isModLoaded("farmersdelight")) {
+                            entries.add(BlockRegistry.STACKED_ORGANIC_COMPOST);
+                        }
 
-                if (ModList.get().isLoaded("farmersdelight")) {
-                    output.accept(BlockRegistry.STACKED_ORGANIC_COMPOST.get());
-                }
+                        entries.add(BlockRegistry.STACKED_MELONS);
+                        entries.add(BlockRegistry.STACKED_PUMPKINS);
+                        entries.add(BlockRegistry.STACKED_OAK_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_OAK_LOGS);
+                        entries.add(BlockRegistry.STACKED_SPRUCE_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_SPRUCE_LOGS);
+                        entries.add(BlockRegistry.STACKED_BIRCH_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_BIRCH_LOGS);
+                        entries.add(BlockRegistry.STACKED_JUNGLE_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_JUNGLE_LOGS);
+                        entries.add(BlockRegistry.STACKED_ACACIA_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_ACACIA_LOGS);
+                        entries.add(BlockRegistry.STACKED_DARK_OAK_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_DARK_OAK_LOGS);
+                        entries.add(BlockRegistry.STACKED_MANGROVE_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_MANGROVE_LOGS);
+                        entries.add(BlockRegistry.STACKED_CHERRY_LOGS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_CHERRY_LOGS);
+                        entries.add(BlockRegistry.STACKED_BAMBOO_BLOCKS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_BAMBOO_BLOCKS);
+                        entries.add(BlockRegistry.STACKED_CRIMSON_STEMS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_CRIMSON_STEMS);
+                        entries.add(BlockRegistry.STACKED_WARPED_STEMS);
+                        entries.add(BlockRegistry.STACKED_STRIPPED_WARPED_STEMS);
+                    })
+                    .build());
 
-                output.accept(BlockRegistry.STACKED_MELONS.get());
-                output.accept(BlockRegistry.STACKED_PUMPKINS.get());
-                output.accept(BlockRegistry.STACKED_OAK_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_OAK_LOGS.get());
-                output.accept(BlockRegistry.STACKED_SPRUCE_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_SPRUCE_LOGS.get());
-                output.accept(BlockRegistry.STACKED_BIRCH_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_BIRCH_LOGS.get());
-                output.accept(BlockRegistry.STACKED_JUNGLE_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_JUNGLE_LOGS.get());
-                output.accept(BlockRegistry.STACKED_ACACIA_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_ACACIA_LOGS.get());
-                output.accept(BlockRegistry.STACKED_DARK_OAK_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_DARK_OAK_LOGS.get());
-                output.accept(BlockRegistry.STACKED_MANGROVE_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_MANGROVE_LOGS.get());
-                output.accept(BlockRegistry.STACKED_CHERRY_LOGS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_CHERRY_LOGS.get());
-                output.accept(BlockRegistry.STACKED_BAMBOO_BLOCKS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_BAMBOO_BLOCKS.get());
-                output.accept(BlockRegistry.STACKED_CRIMSON_STEMS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_CRIMSON_STEMS.get());
-                output.accept(BlockRegistry.STACKED_WARPED_STEMS.get());
-                output.accept(BlockRegistry.STACKED_STRIPPED_WARPED_STEMS.get());
-
-
-            }).build());
+    public static void registerItemGroups() {
+        StackedBlocks.LOGGER.info("Registering Item Groups for " + StackedBlocks.MODID);
+    }
 }
