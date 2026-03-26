ea2dfba623802080425ab0fb1ce9e8b068efb3a3
axperty <107956734+axperty@users.noreply.github.com>
2026-03-26 00:01:32 -0300

    See changelog.md for the latest changes.

--- src/main/java/com/axperty/stackedblocks/registry/CreativeTabRegistry.java
+++ src/main/java/com/axperty/stackedblocks/registry/CreativeTabRegistry.java
@@ -1,96 +1,97 @@
 package com.axperty.stackedblocks.registry;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import com.axperty.stackedblocks.StackedBlocks;
 
 public class CreativeTabRegistry {
-    public static final ItemGroup STACKEDBLOCKS_ITEMGROUP = Registry.register(Registries.ITEM_GROUP,
+    public static final CreativeModeTab STACKEDBLOCKS_ITEMGROUP = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
             Identifier.tryParse(StackedBlocks.MODID + ":" + "stackedblocks_itemgroup"),
-            FabricItemGroup.builder()
-                    .displayName(Text.translatable("itemGroup.stackedblocks"))
+            FabricCreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.cratedelight"))
                     .icon(() -> new ItemStack(BlockRegistry.STACKED_MELONS))
-                    .entries((displayContext, entries) -> {
+                    .displayItems((displayContext, entries) -> {
 
-                        entries.add(BlockRegistry.STACKED_STONE_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_COBBLESTONE_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_NETHERRACK_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_BRICKS);
-                        entries.add(BlockRegistry.STACKED_RESIN_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_RESIN_BRICKS);
-                        entries.add(BlockRegistry.STACKED_MELONS);
-                        entries.add(BlockRegistry.STACKED_PUMPKINS);
+                        entries.accept(BlockRegistry.STACKED_STONE_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_COBBLESTONE_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_NETHERRACK_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_BRICKS);
+                        entries.accept(BlockRegistry.STACKED_RESIN_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_RESIN_BRICKS);
+                        entries.accept(BlockRegistry.STACKED_MELONS);
+                        entries.accept(BlockRegistry.STACKED_PUMPKINS);
 
-                        entries.add(BlockRegistry.STACKED_RAW_IRON_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_RAW_GOLD_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_RAW_COPPER_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_RAW_IRON_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_RAW_GOLD_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_RAW_COPPER_BLOCKS);
 
-                        entries.add(BlockRegistry.STACKED_LAPIS_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_REDSTONE_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_COAL_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_IRON_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_GOLD_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_EMERALD_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_DIAMOND_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_NETHERITE_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_QUARTZ_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_LAPIS_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_REDSTONE_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_COAL_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_IRON_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_GOLD_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_EMERALD_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_DIAMOND_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_NETHERITE_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_QUARTZ_BLOCKS);
 
-                        entries.add(BlockRegistry.STACKED_OAK_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_OAK_LOGS);
-                        entries.add(BlockRegistry.STACKED_OAK_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_OAK_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_OAK_LOGS);
+                        entries.accept(BlockRegistry.STACKED_OAK_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_SPRUCE_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_SPRUCE_LOGS);
-                        entries.add(BlockRegistry.STACKED_SPRUCE_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_SPRUCE_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_SPRUCE_LOGS);
+                        entries.accept(BlockRegistry.STACKED_SPRUCE_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_BIRCH_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_BIRCH_LOGS);
-                        entries.add(BlockRegistry.STACKED_BIRCH_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_BIRCH_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_BIRCH_LOGS);
+                        entries.accept(BlockRegistry.STACKED_BIRCH_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_JUNGLE_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_JUNGLE_LOGS);
-                        entries.add(BlockRegistry.STACKED_JUNGLE_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_JUNGLE_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_JUNGLE_LOGS);
+                        entries.accept(BlockRegistry.STACKED_JUNGLE_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_ACACIA_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_ACACIA_LOGS);
-                        entries.add(BlockRegistry.STACKED_ACACIA_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_ACACIA_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_ACACIA_LOGS);
+                        entries.accept(BlockRegistry.STACKED_ACACIA_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_DARK_OAK_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_DARK_OAK_LOGS);
-                        entries.add(BlockRegistry.STACKED_DARK_OAK_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_DARK_OAK_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_DARK_OAK_LOGS);
+                        entries.accept(BlockRegistry.STACKED_DARK_OAK_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_MANGROVE_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_MANGROVE_LOGS);
-                        entries.add(BlockRegistry.STACKED_MANGROVE_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_MANGROVE_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_MANGROVE_LOGS);
+                        entries.accept(BlockRegistry.STACKED_MANGROVE_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_CHERRY_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_CHERRY_LOGS);
-                        entries.add(BlockRegistry.STACKED_CHERRY_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_CHERRY_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_CHERRY_LOGS);
+                        entries.accept(BlockRegistry.STACKED_CHERRY_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_PALE_OAK_LOGS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_PALE_OAK_LOGS);
-                        entries.add(BlockRegistry.STACKED_PALE_OAK_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_PALE_OAK_LOGS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_PALE_OAK_LOGS);
+                        entries.accept(BlockRegistry.STACKED_PALE_OAK_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_BAMBOO_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_BAMBOO_BLOCKS);
-                        entries.add(BlockRegistry.STACKED_BAMBOO_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_BAMBOO_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_BAMBOO_BLOCKS);
+                        entries.accept(BlockRegistry.STACKED_BAMBOO_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_CRIMSON_STEMS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_CRIMSON_STEMS);
-                        entries.add(BlockRegistry.STACKED_CRIMSON_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_CRIMSON_STEMS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_CRIMSON_STEMS);
+                        entries.accept(BlockRegistry.STACKED_CRIMSON_PLANKS);
 
-                        entries.add(BlockRegistry.STACKED_WARPED_STEMS);
-                        entries.add(BlockRegistry.STACKED_STRIPPED_WARPED_STEMS);
-                        entries.add(BlockRegistry.STACKED_WARPED_PLANKS);
+                        entries.accept(BlockRegistry.STACKED_WARPED_STEMS);
+                        entries.accept(BlockRegistry.STACKED_STRIPPED_WARPED_STEMS);
+                        entries.accept(BlockRegistry.STACKED_WARPED_PLANKS);
                     })
                     .build());
 
     public static void registerItemGroups() {
+        StackedBlocks.LOGGER.info("Registering creative mode tab for " + StackedBlocks.MODID);
     }
 }
